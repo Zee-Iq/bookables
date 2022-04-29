562faa100f3cd53be64d99114261bc5744ad34af
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-04-29 11:58:25 +0200

    Creating slices

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Router, Route } from "react-router-dom";
+import "./App.css";
+import { useAppDispatch, useAppSelector } from "./hooks";
+import { selectCounter, increment, decrement, incrementByAmount } from "./slices/counterSlice";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const counter = useAppSelector(selectCounter);
   return (
     <div className="App">
-   
+      Counter: {counter}
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(10))}>inc.incrementByAmount</button>
     </div>
   );
 }
