e23fb39fd85aa1a40526671451be5f2e72beabaa
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-04-29 11:25:40 +0200

    Create Interfaces

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,13 +1,69 @@
 declare namespace Bookables {
-    interface User {
-      email: string;
-      password: string;
-      doesSomething(): number;
-      phoneNumber: string;
-    }
-    interface SpecialUser extends User {
-      somethingElse: string;
-    }
+  type UserRole = "tenant" | "host";
+
+  interface PayoutInformation {
+    bic: string;
+    iban: string;
+    owner: string;
   }
 
-export default Bookables
+  type PaymentProvider = {
+    provider: "creditcard";
+    cardnumber: number;
+    validationNumber: number;
+    owner: string;
+    expiration: Date;
+  };
+
+  interface Email {
+    address: string;
+    isConfirmed: boolean;
+  }
+  interface User {
+    email: Email;
+    password: string;
+    roles: UserRole[];
+    paymentProviders?: PaymentProvider[];
+    payoutInformation?: PayoutInformation;
+  }
+
+  interface Tenant extends User {
+    roles: ["tenant"]
+    paymentProviders: [PaymentProvider]
+  }
+
+  interface Host extends User {
+    roles: ["host"]
+    payoutInformation: PayoutInformation;
+  }
+
+  type BookableType = "seat" | "room";
+  interface Bookable {
+    id: string;
+    type: BookableType;
+    equipment:String[]
+  }
+
+  interface ContactInformation {
+    email: string;
+    phoneNumber?: string;
+  }
+
+  interface Address {
+    countryCode: string;
+    postalCode: string;
+    streetName: string;
+    houseNumber: string;
+    name: string;
+  }
+  interface Space {
+    owner: Host;
+    name: string;
+    description: string;
+    address: Address;
+    contactInformation: ContactInformation;
+  }
+
+}
+
+export default Bookables;
