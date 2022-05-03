6f8ad155e8e5cda683f8b3cfb42851bc6ec2c2a1
MiloszSzymanek <szymanekmilosz92@gmail.com>
2022-05-03 12:16:26 +0200

    user registration works

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Router, Route } from "react-router-dom";
+import Register from './components/register/Register';
 
 function App() {
   return (
     <div className="App">
+      <Register />
    
     </div>
   );
--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,13 +1,22 @@
 import express from "express"
 import Bookables from "types"
+import mongoose from "mongoose";
+import env from "./config/env";
 
 
+mongoose.connect(env.MONGO_URI, () => console.log("mongo DB is connected")
+)
 
 const app = express()
+app.use('/users',require('./api/users') );
 
-app.get("/greeting", (req, res) => {
+
+ app.get("/greeting", (req, res) => {
     res.json({ "message": "Hello World" })
-})
+}) 
+
+
 
 
 app.listen(4000, () => { console.log(" App running on 4000") })
+
