595d62a2b396956e5bfaeca7c6c5ecf64b989d13
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-05-03 13:22:28 +0200

    Set up store.ts

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,16 +1,12 @@
 import "./App.css";
-import { useAppDispatch, useAppSelector } from "./hooks";
-import { selectCounter, increment, decrement, incrementByAmount } from "./slices/counterSlice";
+
+
 
 function App() {
-  const dispatch = useAppDispatch();
-  const counter = useAppSelector(selectCounter);
+
   return (
     <div className="App">
-      Counter: {counter}
-      <button onClick={() => dispatch(increment())}>increment</button>
-      <button onClick={() => dispatch(decrement())}>decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(10))}>inc.incrementByAmount</button>
+      <div>test</div>
     </div>
   );
 }
