a60efffe46c6c34576c23177e7562c8e09eaabb2
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-05-17 19:25:15 +0200

    Changed Session Key import

--- Client/src/slices/filterSlice.tsx
+++ Client/src/slices/filterSlice.tsx
@@ -1,18 +1,11 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction, AsyncThunkPayloadCreator} from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import Bookables from "types";
-
-type Latitude = number;
-type Longitude = number;
-
-type Point = {
-  type: "Point";
-  coordinates: [Latitude, Longitude];
-};
+import { getMapSession } from "../components/map/Map";
 
 type Location = {
   name: string;
-  point: Point;
+  point: Bookables.Point;
   address: Partial<{
     addressLine: string;
     locality: string;
@@ -25,6 +18,7 @@ type Location = {
     countryRegionIso2: string;
     landmark: string;
   }>;
+  confidence: "Low" | "Medium" | "High"
 };
 
 export type LabeledLocation = { label: string } & Location;
@@ -105,7 +99,6 @@ const filterReducer = filterSlice.reducer;
 export default filterReducer;
 
 // exporting selectors
-
 export const selectFilters = (state: RootState) => state.filter;
 
 // exporting actions
@@ -114,9 +107,10 @@ const fetchAutosuggest = createAsyncThunk(
   "filter/fetchAutosuggest",
   async (query: string, thunkApi) => {
     const encodedQuery = encodeURIComponent(query)
-    const response = await fetch(`http://dev.virtualearth.net/REST/v1/Locations?key=${process.env.REACT_APP_BING_MAPS}&query=${encodedQuery}`);
+    const key = (await getMapSession()) || process.env.REACT_APP_BING_MAPS
+    const response = await fetch(`http://dev.virtualearth.net/REST/v1/Locations?key=${key}&query=${encodedQuery}&maxResults=${20}`);
     const locationSuggestions: Location[] = (await response.json()).resourceSets[0]?.resources;
-    return locationSuggestions.map((suggestion) => ({
+    return locationSuggestions.filter(suggestion => suggestion.confidence !== "Low").map((suggestion) => ({
       label: suggestion.name,
       ...suggestion,
     }));
