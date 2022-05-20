1f475b8d853efbf886fe8c9c0d81ca37cb186ce7
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-05-20 13:37:44 +0200

    added AppStyles  (our own custom style)

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,18 +1,22 @@
+import { Box } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Layout from "./Components/Layout";
+import AppStyles from "./AppStyles";
 
 import YourSpaces from "./Components/YourSpaces/YourSpaces";
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="yourspace" element={<YourSpaces />} />
-        </Route>
-      </Routes>
+      <Box sx={AppStyles}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="yourspace" element={<YourSpaces />} />
+          </Route>
+        </Routes>
+      </Box>
     </div>
   );
 };
