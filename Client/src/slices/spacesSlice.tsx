b8b240c757eb1dab079a31ec15fb0fa7c3f1c35e
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-05-20 13:41:56 +0200

    Remove fake Space

--- Client/src/slices/spacesSlice.tsx
+++ Client/src/slices/spacesSlice.tsx
@@ -13,25 +13,7 @@ interface SpacesState {
 }
 
 const initialState: SpacesState = {
-  spacesInArea: [
-    {
-      address: {
-        countryCode: "DE",
-        houseNumber: "20",
-        name: "Jimmys",
-        postalCode: "46540",
-        streetName: "Bla",
-      },
-      contactInformation: { email: "k@k.de", phoneNumber: "123456" },
-      description: "dwadwa",
-      name: "dwadwadwa",
-      owner: "dwadwa",
-      point: {
-        type: "Point",
-        coordinates: [51.4821662902832, 11.96579647064209],
-      },
-    },
-  ],
+  spacesInArea: [],
   selectedSpace: null,
 };
 
