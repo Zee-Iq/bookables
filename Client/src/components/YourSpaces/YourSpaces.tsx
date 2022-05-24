c7711e6c20404c2aba7325170325b94f64ee4b2b
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-05-24 14:49:50 +0200

    Corrected import spelling

---
+++ Client/src/components/YourSpaces/YourSpaces.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const YourSpaces = () => {
+  return (
+    <div>
+      <h1>Hello from YourSpaces.tsx</h1>
+      <p>
+        <Link to="/">Layout</Link>
+      </p>
+    </div>
+  );
+};
+
+export default YourSpaces;
