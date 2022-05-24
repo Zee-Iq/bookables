6aa3125c763ec2bf888d103b26510017caf0e225
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-05-24 13:39:36 +0200

    created yourspaces boiler plate

--- Client/src/Components/YourSpaces/YourSpaces.tsx
+++ Client/src/Components/YourSpaces/YourSpaces.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const YourSpaces = () => {
   return (
     <div>
-      <h1>Hello from SingleSpace.tsx</h1>
+      <h1>Hello from YourSpaces.tsx</h1>
       <p>
         <Link to="/">Layout</Link>
       </p>
