2cdcc250db906ba7a196fbc8a14623112db19ee3
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-05-27 21:41:58 +0200

    Add YourSpaces Component

--- Client/src/components/YourSpaces/YourSpaces.tsx
+++ Client/src/components/YourSpaces/YourSpaces.tsx
@@ -1,14 +1,41 @@
-import { AppBar, Box, Drawer, List, ListItem, ListItemButton, Toolbar, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Box, Divider, List, ListItemButton, Paper } from "@mui/material";
+import { useEffect, useState } from "react";
+import Bookables from "types";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { fetchSpacesForHost, seletOwnedSpaces } from "../../slices/spacesSlice";
 import SpaceEditor from "../SpaceEditor/SpaceEditor";
+import AddIcon from "@mui/icons-material/Add";
 
 const YourSpaces = () => {
+  const [selectedSpace, setSelectedSpace] = useState<Bookables.Space>();
+  const dispatch = useAppDispatch();
+  const spaces = useAppSelector(seletOwnedSpaces);
+
+  useEffect(() => {
+    if (spaces.length > 0) setSelectedSpace(spaces[0]);
+  }, [spaces]);
+
+  useEffect(() => {
+    dispatch(fetchSpacesForHost());
+  }, [dispatch]);
   return (
-    <Box sx={{ display: 'flex' }}>
-      <List>
-        <ListItem>Space</ListItem>
-      </List>
-      <SpaceEditor/>
+    <Box sx={{ display: "grid", gridTemplateColumns: "max-content 1fr", gap: 1, minHeight: "100%", pb:1 }}>
+      <Paper>
+        <List>
+          {spaces.map((space) => (
+            <ListItemButton
+              key={space.name}
+              onClick={() => setSelectedSpace(space)}
+            >
+              {space.name}
+            </ListItemButton>
+          ))}
+          <ListItemButton onClick={() => setSelectedSpace(undefined)}>
+            <AddIcon /> new Space
+          </ListItemButton>
+        </List>
+      </Paper>
+      <SpaceEditor space={selectedSpace} />
     </Box>
   );
 };
