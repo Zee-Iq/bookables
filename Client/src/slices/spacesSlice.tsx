2a08b2393f669cf17291a75f5cf2edff11f975f7
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-05-27 21:41:07 +0200

    Implement Fetch Spaces for Host

--- Client/src/slices/spacesSlice.tsx
+++ Client/src/slices/spacesSlice.tsx
@@ -2,46 +2,65 @@ import {
   createAsyncThunk,
   createSlice,
   PayloadAction,
-  AsyncThunkPayloadCreator,
 } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import Bookables from "types";
+import axios from "axios";
 
 interface SpacesState {
-  spaces: Bookables.Space[];
+  spacesInArea: Bookables.Space[];
+  ownedSpaces: Bookables.Space[];
   selectedSpace: Bookables.Space | null;
 }
 
 const initialState: SpacesState = {
-  spaces: [],
+  spacesInArea: [],
+  ownedSpaces: [],
   selectedSpace: null,
 };
 
 const spacesSlice = createSlice({
   name: "spaces",
   initialState,
-  reducers: {},
+  reducers: {
+    setOwnedSpaces(state, action: PayloadAction<Bookables.Space[]>) {
+      state.ownedSpaces = action.payload;
+    },
+  },
 });
 
-export const fetchSpacesForHost = createAsyncThunk<Bookables.Space[]>("spaces/fetchSpacesForHost", () => {
-  console.warn("User should come from userSlice, still needs to be implemented.")
-  const user = {
-    _id: "628ca4a90b48ec82d91b06ec",
-    email: { isConfirmed: true, address: "szymanekmilosz92@gmail.com" },
-    roles: ["host", "tenant"]
+export const fetchSpacesForHost = createAsyncThunk(
+  "spaces/fetchSpacesForHost",
+  async (_, thunkApi) => {
+    const { dispatch } = thunkApi;
+    const { setOwnedSpaces: setSpaces } = spacesSlice.actions;
+    console.warn(
+      "User should come from userSlice, still needs to be implemented."
+    );
+    const user = {
+      _id: "628ca4a90b48ec82d91b06ec",
+      email: { isConfirmed: true, address: "szymanekmilosz92@gmail.com" },
+      roles: ["host", "tenant"],
+    };
+    console.warn(
+      "token should come from userSlice, still needs to be implemented."
+    );
+    const token = "dwadowaijhowaifwaoindwa"
+
+    if (!user || !user.roles.includes("host")) dispatch(setSpaces([]));
+    const response = await axios.get<Bookables.Space[]>("spaces/owned", {headers: {"authorization": `bearer ${token}`}, validateStatus: () => true})
+    if(response.status === 200) return dispatch(setSpaces(response.data))
+    dispatch(setSpaces([]))
   }
-
-  if(!user || !user.roles.includes("host")) return [];
-  return []
-})
-
+);
 
 // exporting filterReducer
 const spacesReducer = spacesSlice.reducer;
 export default spacesReducer;
 
 // exporting selectors
-export const selectSpaces = (state: RootState) => state.spaces.spaces;
+export const selectSpacesInArea = (state: RootState) => state.spaces.spacesInArea;
+export const seletOwnedSpaces = (state: RootState) => state.spaces.ownedSpaces;
 
 // exporting actions
 export const {} = spacesSlice.actions;
