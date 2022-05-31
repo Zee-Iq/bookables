496ece166cd7c728382e7de802ba48eca3e2bb94
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-05-31 21:42:50 +0200

    added actions for login

--- Client/src/components/login/Login.tsx
+++ Client/src/components/login/Login.tsx
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
+import { login, selectUser, selectLoginInError } from "../../slices/userSlice";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { Navigate } from "react-router-dom";
 
 export default function Login() {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector(selectUser);
+  const loginError = useAppSelector(selectLoginInError);
+
   const [data, setData] = useState({
     email: "",
     password: "",
   });
+  console.log(user)
+  // if there is a user, login is sucessfull --> show homepage
+  if (user) {
+    return <Navigate to="/" />;
+  }
 
   const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (!data.password || !data.email) return;
-    const response = await axios.post("/users/login", data);
+    dispatch(login({email:data.email, password:data.password}))
   };
 
   return (
