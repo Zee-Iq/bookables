7db5c4928e1cc815b487bc63f74af5ec377c65b9
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-06-06 22:37:28 +0200

    Added AccordeonMobile and Mobile Drawer

--- Client/src/components/Home/Home.tsx
+++ Client/src/components/Home/Home.tsx
@@ -1,9 +1,13 @@
 import { Box } from "@mui/material";
 import Map from "../Map/Map";
+import AccordeonMobile from "../Mobile/AccordeonMobile";
+import MobileDrawer from "../Mobile/MobileDrawer";
 import Search from "../Search/Search";
 const Home = () => {
   return (
     <Box>
+    <AccordeonMobile />
+    <MobileDrawer />
     <Search />
     <Map />
     </Box>
