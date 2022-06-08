a4a34303b96088755f2e9bbc399dbffc50218048
MiloszSzymanek <szymanekmilosz92@gmail.com>
2022-06-08 18:14:07 +0200

    confirmation Modal changed

---
+++ Client/src/components/confirmEmailModal/CofirmEmailModal.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { useEffect } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useAppSelector } from "../../hooks";
+import { selectUser } from "../../slices/userSlice";
+import axios from "axios";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ConfirmEmailModal() {
+  const user = useAppSelector(selectUser);
+
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    const getUpdatedUser = async () => {
+      const response = await axios.post("/users/updatedUser", {
+        _id: user?._id,
+      });
+
+      console.log("response is", response);
+
+      if (response.data.user?.email.isConfirmed === false) {
+        setOpen(true);
+      }
+    };
+    getUpdatedUser();
+  }, []);
+
+  console.log("user is", user?.email.isConfirmed);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Please confirm your email address
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            We have sent a verification message to your e-mail address.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
