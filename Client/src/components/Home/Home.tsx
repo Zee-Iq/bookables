c923001e0adb4c991e3d9ccfad4fa78a0b7aaaa1
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-06-08 11:28:02 +0200

    removed accordion

--- Client/src/components/Home/Home.tsx
+++ Client/src/components/Home/Home.tsx
@@ -1,12 +1,10 @@
 import { Box } from "@mui/material";
 import Map from "../Map/Map";
-import AccordeonMobile from "../Mobile/AccordeonMobile";
 import MobileDrawer from "../Mobile/MobileDrawer";
 import Search from "../Search/Search";
 const Home = () => {
   return (
     <Box>
-    <AccordeonMobile />
     <MobileDrawer />
     <Search />
     <Map />
